cc0bab264c849326b4bc30e02e32f42dfd3f6f2b
Samuel Lassoie <samuel.lassoie@gmail.com>
2018-03-01 23:28:07 +0100

    remove client

--- client_supprimer.php
+++ client_supprimer.php
@@ -4,8 +4,12 @@ include('connect.php');
 $id = $_GET['id'] ?? null;
 if(null !== $id) {
 $_SESSION['suppression_client'] = "le client a été supprimé";
+//preparer la requête
+$suppressionSth =
+$connexion->prepare('delete from clients where id_client = :id');
+$suppressionSth->bindParam('id', $id, PDO::PARAM_INT);
 
-
+$suppressionSth->execute();
 }
 
 header('Location: client.php');
