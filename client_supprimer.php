7239495972210f473f596791e35a51306f038a44
Samuel Lassoie <samuel.lassoie@gmail.com>
2018-03-03 11:08:36 +0100

    add doc and display request 2018_03_01

--- client_supprimer.php
+++ client_supprimer.php
@@ -1,15 +1,32 @@
 <?php
+//démarre une nouvelle session ou reprend une session existante
 session_start();
+//recuperation de la connexion à la BDD
 include('connect.php');
+//récupération de l'id de l'enregistrement que l'on va supprimer
 $id = $_GET['id'] ?? null;
+//si l'id n'est pas null, on va supprimer l'enregistrement
 if(null !== $id) {
-$_SESSION['suppression_client'] = "le client a été supprimé";
-//preparer la requête
+//le template de la requête sql
+$requeteSuppClient = "delete from clients where id_client = :id";
+//preparation de la requête
 $suppressionSth =
-$connexion->prepare('delete from clients where id_client = :id');
+$connexion->prepare($requeteSuppClient);
+//liaison du nom ':id' à la variable id
 $suppressionSth->bindParam('id', $id, PDO::PARAM_INT);
 
+//si la suppression a fonctionné, on ajoute un message
 $suppressionSth->execute();
+
+//Retourne le nombre de lignes affectées par le dernier appel à la fonction PDOStatement::execute()
+$rowCount = $suppressionSth->rowCount();
+
+//s'il y a au moins une ligne qui a été supprimée on indique un message
+if(0 < $rowCount) {
+  $_SESSION['suppression_client'] = "le client a été supprimé";
 }
 
+}
+
+//on redirige vers la page client
 header('Location: client.php');
--- readme.md
+++ readme.md
@@ -8,6 +8,8 @@
 - [Cours du 01/03/2018](docs/lesson-2018_03_01.md)
 
 ## Requête SQL
+
 - [Requêtes du 25/01/2018](sql_request/request-2018_01_25.md)
 - [Requêtes du 08/02/2018](sql_request/request-2018_02_08.md)
 - [Requêtes du 22/02/2018](sql_request/request-2018_02_22.md)
+- [Requêtes du 01/03/2018](sql_request/request-2018_03_01.md)
